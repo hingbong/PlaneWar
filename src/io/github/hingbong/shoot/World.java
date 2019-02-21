880151864441c48c53db02dd74cec776ebc524c4
hingbong <hingbonglo@gmail.com>
2019-02-21 10:32:28 +0800

    to be visible

--- src/io/github/hingbong/shoot/World.java
+++ src/io/github/hingbong/shoot/World.java
@@ -10,29 +10,36 @@ public class World extends JPanel {
   Hero hero = Hero.hero;
   Sky sky = Sky.sky;
   FlyObject[] flies = new FlyObject[3];
-  Bullet bullet = new Bullet(120, 350);
+  Bullet[] bullet = new Bullet[1];
 
   public void start() {
     flies[0] = new AirPlane();
     flies[1] = new Bee();
     flies[2] = new BigPlane();
+    bullet[0] = new Bullet(120, 350);
   }
 
   @Override
   public void paint(Graphics g) {
     sky.paintObject(g);
     hero.paintObject(g);
-
+    for (FlyObject f : flies) {
+      f.paintObject(g);
+    }
+    for (Bullet b : bullet) {
+      b.paintObject(g);
+    }
   }
 
   public static void main(String[] args) {
     JFrame frame = new JFrame("Shoot Game");// initialize the window
     World w = new World();// run start method in main method
     frame.add(w);// add world to window
+    w.start();
     frame.setSize(400, 700);// set the window size
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// set the (x)close function
     frame.setLocationRelativeTo(null);// window initialized location
     frame.setVisible(true);// display the window
-    w.start();
+    
   }
 }
