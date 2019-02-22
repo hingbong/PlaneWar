305e0b9015d11b9ddc210d5513f0f86d2c75abd7
hingbong <hingbonglo@gmail.com>
2019-02-22 11:40:06 +0800

    add newEnemy()

--- src/io/github/hingbong/shoot/World.java
+++ src/io/github/hingbong/shoot/World.java
@@ -14,14 +14,21 @@ public class World extends JPanel {
   // the unit in the world
   Hero hero = Hero.hero;
   Sky sky = Sky.sky;
-  FlyObject[] flies = new FlyObject[3];
-  Bullet[] bullet = new Bullet[1];
+  FlyObject[] enemies = new FlyObject[0];
+  Bullet[] bullet = new Bullet[0];
+
+  // generate enemy
+  public FlyObject newEnemy() {
+    int number = (int) (Math.random() * 100);
+    if (number < 20) {
+      return new Bee();
+    } else if (number < 55) {
+      return new AirPlane();
+    } else
+      return new BigPlane();
+  }
 
   public void start() {
-    flies[0] = new AirPlane();
-    flies[1] = new Bee();
-    flies[2] = new BigPlane();
-    bullet[0] = new Bullet(120, 350);
     // set a timer
     Timer timer = new Timer();
     // set interval
@@ -30,7 +37,7 @@ public class World extends JPanel {
 
       @Override
       public void run() {
-        
+
       }
     };
     timer.schedule(task, interval, interval);
@@ -40,7 +47,7 @@ public class World extends JPanel {
   public void paint(Graphics g) {
     sky.paintObject(g);
     hero.paintObject(g);
-    for (FlyObject f : flies) {
+    for (FlyObject f : enemies) {
       f.paintObject(g);
     }
     for (Bullet b : bullet) {
