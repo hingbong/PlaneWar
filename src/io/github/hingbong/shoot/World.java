81a090aceb55d10ffb19fa5cbcf0b7b83cfe2e16
hingbong <hingbonglo@gmail.com>
2019-02-22 15:16:42 +0800

    let things move

--- src/io/github/hingbong/shoot/World.java
+++ src/io/github/hingbong/shoot/World.java
@@ -9,8 +9,8 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class World extends JPanel {
-  public static final int WIDTH = 400;
-  public static final int HEIGHT = 700;
+  public static final int WIDTH = 480;
+  public static final int HEIGHT = 852;
 
   // the unit in the world
   Hero hero = Hero.hero;
@@ -33,10 +33,31 @@ public class World extends JPanel {
 
   public void enterAction() {
     enterIndex++;
-    if (enterIndex % 50 == 0) {
+    if (enterIndex % 40 == 0) {
       FlyObject enemy = newEnemy();
-      enemies = Arrays.copyOf(enemies, enemies.length);
+      enemies = Arrays.copyOf(enemies, enemies.length + 1);
       enemies[enemies.length - 1] = enemy;
+    }
+  }
+
+  int shootIndex = 0;
+
+  public void shootAction() {
+    shootIndex++;
+    if (shootIndex % 20 == 0) {
+      Bullet[] bs = hero.shoot();
+      bullet = Arrays.copyOf(bullet, bullet.length + bs.length);
+      System.arraycopy(bs, 0, bullet, bullet.length - bs.length, bs.length);
+    }
+  }
+
+  public void stepAction() {
+    sky.step();
+    for (FlyObject f : enemies) {
+      f.step();
+    }
+    for (Bullet b : bullet) {
+      b.step();
     }
   }
 
@@ -49,22 +70,25 @@ public class World extends JPanel {
 
       @Override
       public void run() {
-
+        enterAction();
+        shootAction();
+        stepAction();
+        repaint();
       }
     };
-    timer.schedule(task, interval, interval);
+    timer.schedule(task, 0, interval);
   }
 
   @Override
   public void paint(Graphics g) {
     sky.paintObject(g);
-    hero.paintObject(g);
     for (FlyObject f : enemies) {
       f.paintObject(g);
     }
     for (Bullet b : bullet) {
       b.paintObject(g);
     }
+    hero.paintObject(g);
   }
 
   public static void main(String[] args) {
